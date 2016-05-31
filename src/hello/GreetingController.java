e3262e524212dbf20e0ca1f4fde6181bac94f51c
developer-aj <ajju.kvmc@gmail.com>
2016-06-01 02:12:44 +0530

    adding JSON response

--- src/hello/GreetingController.java
+++ src/hello/GreetingController.java
@@ -2,18 +2,18 @@ package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-import hello.Greeting;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class GreetingController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 	
-	@RequestMapping("/greeting")
-	public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name){
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	 @RequestMapping(value = "greeting", method = RequestMethod.GET, headers = "Accept=application/json")
+	 public @ResponseBody Greeting greeting(){
+		return new Greeting(counter.incrementAndGet(), String.format(template, "World"));
 	}
 }
