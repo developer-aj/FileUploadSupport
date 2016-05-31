34acaaeaacae087417fb6e8f3d1f279a202a3b77
developer-aj <ajju.kvmc@gmail.com>
2016-06-01 02:19:18 +0530

    Using request params

--- src/hello/GreetingController.java
+++ src/hello/GreetingController.java
@@ -2,6 +2,7 @@ package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,8 +13,12 @@ public class GreetingController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 	
-	 @RequestMapping(value = "greeting", method = RequestMethod.GET, headers = "Accept=application/json")
-	 public @ResponseBody Greeting greeting(){
+	 @RequestMapping(value = "greeting/{name}", method = RequestMethod.GET, headers = "Accept=application/json")
+	 public @ResponseBody Greeting greeting(@PathVariable String name){
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+	@RequestMapping(value = "greeting/", method = RequestMethod.GET, headers = "Accept=application/json")
+	public @ResponseBody Greeting greeting(){
 		return new Greeting(counter.incrementAndGet(), String.format(template, "World"));
 	}
 }
