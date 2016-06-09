bc6fd7185d457a6fca87e27d761f5a474c75ef3a
developer-aj <ajju.kvmc@gmail.com>
2016-06-10 00:32:00 +0530

    Basic hello world example

--- src/hello/GreetingController.java
+++ src/hello/GreetingController.java
@@ -1,24 +1,18 @@
 package hello;
 
-import java.util.concurrent.atomic.AtomicLong;
-
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class GreetingController {
-	private static final String template = "Hello, %s!";
-	private final AtomicLong counter = new AtomicLong();
-	
-	 @RequestMapping(value = "greeting/{name}", method = RequestMethod.GET, headers = "Accept=application/json")
-	 public @ResponseBody Greeting greeting(@PathVariable String name){
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
-	}
-	@RequestMapping(value = "greeting/", method = RequestMethod.GET, headers = "Accept=application/json")
-	public @ResponseBody Greeting greeting(){
-		return new Greeting(counter.incrementAndGet(), String.format(template, "World"));
-	}
+@Controller
+@RequestMapping("/hello")
+public class GreetingController{
+ 
+   @RequestMapping(method = RequestMethod.GET)
+   public String printHello(ModelMap model) {
+      model.addAttribute("message", "Hello Spring MVC Framework!");
+      return "hello";
+   }
+
 }
